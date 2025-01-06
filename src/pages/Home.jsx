4c8b7998bc5884d7d5227edaf0e2a017da41a2ab
dawoodkaleem <dawood9743@gmail.com>
2025-01-06 20:06:53 +0500

    Hero section image and css created

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ export const Home = () => {
       <div className="container grid grid-two-cols">
         <div className="hero-content">
           <h1 className=" heading-xl">
-            Explore the World, One Country at a Time
+            Explore the World,One Country at a Time
           </h1>
           <p className="paragraph">
             Discover the history, culture, and beauty of every
@@ -17,7 +17,13 @@ export const Home = () => {
             Start Exploring <FaLongArrowAltRight />
           </button>
         </div>
-        <div className="hero-image"></div>
+        <div className="hero-image">
+          <img
+            src="/images/world.png"
+            alt="world beauty"
+            className="banner-image"
+          />
+        </div>
       </div>
     </main>
   );
