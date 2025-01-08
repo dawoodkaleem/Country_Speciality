87f932f7e668af5b3fd2650c8af082a054fcd645
dawoodkaleem <dawood9743@gmail.com>
2025-01-08 19:28:06 +0500

    Footer completed

--- src/components/UI/Footer.jsx
+++ src/components/UI/Footer.jsx
@@ -1,7 +1,25 @@
+import footerContact from "../../api/footerApi.json";
+import { IoCallSharp } from "react-icon/io5";
 export const Footer = () => {
+  const footerIcon = {
+    MdPlace: <MdPlace />,
+  };
   return (
-    <>
-      <h1>Hello Footer</h1>
-    </>
+    <footer className="footer-section ">
+      <div className="container grid grid-three-cols ">
+        {footerContact.map((curData, index) => {
+          const { icon, title, details } = curData;
+          return (
+            <div className="footer-contact" key={index}>
+              <div className="icon">{footerIcon.MdPlace}</div>
+              <div className="footer-contact-text">
+                <p>{title}</p>
+                <p>{details}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </footer>
   );
 };
