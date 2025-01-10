e38677bf52939beb6e2e09733e9eb3c0133c2d30
dawoodkaleem <dawood9743@gmail.com>
2025-01-10 21:12:06 +0500

    Layzing loading added

--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -1,7 +1,54 @@
+import { useEffect, useState, ,lazy,useTransition } from "react";
+import { getCountryData } from "../api/postApi";
+import { Loader } from "../components/UI/Loader";
+const SearchFilter = lazy(() => import("../components/UI/SearchFilter"));
+const CountryCard = lazy(() => import("../components/Layout/CountryCard"));
+
 export const Country = () => {
+  const [isPending, startTransition] = useTransition();
+  const [countries, setCountries] = useState([]);
+  const [search, setSearch] = useState();
+  const [filter, setFilter] = useState("all");
+  useEffect(() => {
+    startTransition(async () => {
+      const res = await getCountryData();
+      setCountries(res.data);
+    });
+  }, []);
+  if (isPending) return <Loader />;
+  const searchCountry = (country) => {
+    if (search) {
+      return country.name.common.toLowerCase().includes(search.toLowerCase());
+    }
+    return country;
+  };
+
+  const filterRegion = (country) => {
+    if (filter === "all") return country;
+    return country.region === filter;
+  };
+
+  // here is the main logic
+  const filterCountries = countries.filter(
+    (country) => searchCountry(country) && filterRegion(country)
+  );
+
   return (
-    <>
+    <section className="country-section">
+      <SearchFilter
+        search={search}
+        setSearch={setSearch}
+        filter={filter}
+        setFilter={setFilter}
+        countries={countries}
+        setCountries={setCountries}
+      />
+      <ul className="grid grid-four-cols">
+        {filterCountries.map((curCountry, index) => {
+          return <CountryCard country={curCountry} key={index} />;
+        })}
+      </ul>
       <h1>Country Page</h1>
-    </>
+    </section>
   );
 };
