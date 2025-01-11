c57ea7f214a90f1bd43945ec594721ed1e9d8cad
dawoodkaleem <dawood9743@gmail.com>
2025-01-11 19:50:35 +0500

    error resolved now it is showing it on

--- src/components/UI/Loader.jsx
+++ src/components/UI/Loader.jsx
@@ -1,9 +1,17 @@
 import "../../App.css";
-
+import ClipLoader from "react-spinners/ClipLoader";
 export function Loader() {
+  const [loading, setLoading] = useState(true);
   return (
     <div className="container loader-section">
-      <div className="loader"></div>;
+      <div className="loader">
+        <ClipLoader
+          color="white" // spinner color
+          loading={loading} // controls visibility
+          size={50} // size in px
+        />
+      </div>
+      ;
     </div>
   );
 }
